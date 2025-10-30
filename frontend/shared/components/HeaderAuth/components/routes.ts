78aa2ef904ef3bf3a826d1b82f012a6950517b7a
tay52636 <phuongtay52636@gmail.com>
2025-10-30 08:33:05 +0700

    feat(preview) : UI feature-based

--- frontend/shared/components/HeaderAuth/components/routes.ts
+++ frontend/shared/components/HeaderAuth/components/routes.ts
@@ -14,10 +14,9 @@ export const navItems = [
   { href: "/", label: "Trang chủ", icon: Home },
   { href: "/about", label: "Về chúng tôi", icon: Factory },
   { href: "/explore", label: "Khám phá", icon: Compass },
-  // { href: "/food-recognition", label: "Nhận diện món", icon: Camera },
   { href: "/map", label: "Bản đồ", icon: MapPin },
   { href: "/livestream", label: "Livestream", icon: Video },
-  { href: "/ar-preview", label: "AR Preview", icon: Cube },
+  { href: "/preview", label: "AR Preview", icon: Cube },
   { href: "/events", label: "Sự kiện", icon: Calendar },
 ]
 
